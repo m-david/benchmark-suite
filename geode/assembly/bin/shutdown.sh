90c90e92121bccd888b7975439058672cf8853f6
m-david <manny.david@hazelcast.com>
2018-12-19 16:26:53 -0800

    gfsh no longer available

--- geode/assembly/bin/shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-PRG="$0"
-PRGDIR=`dirname "$PRG"`
-APP_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
-
-gfsh -e "connect --locator=10.212.1.116[10680]" -e "shutdown --include-locators=true"

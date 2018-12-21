3c1f09bf72c65965c20e60747842c260ab83b781
m-david <manny.david@hazelcast.com>
2018-12-20 20:33:25 -0800

    build scripts

--- build-zipall.sh
+++ build-zipall.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
+if [ $1x = "cleanx" ]; then
+  CLEAN_ARG="clean"
+else
+  CLEAN_ARG=""
+fi
+
+COMMAND_LINE="gradle -Dorg.gradle.daemon=false \
+$CLEAN_ARG \
+hazelcast:buildAll \
+geode:buildAll \
+coherence:buildAll \
+ignite:buildAll"
+
 if [ ! -d "./build" ]; then
   mkdir -p "./build"
   echo "created ./build directory"
 else
   echo "./build directory already exists"
 fi
+
+echo $COMMAND_LINE
+$COMMAND_LINE
 
 COMMAND_LINE="tar czvf ./build/benchmark-suite.tar.gz \
  ./geode/build/distributions/benchmark.geode-1.0-SNAPSHOT.tar \
